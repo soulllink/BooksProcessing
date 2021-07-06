21681972c1e3e022b4ec3f102ee728bcf07a005e
soulllink <soullinkslsl@yandex.ru>
2021-07-07 02:01:24 +0600

    fixed txss

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use regex::Regex;
-use rusqlite::{Connection, NO_PARAMS};
+use rusqlite::*;
+//use rusqlite::Connection::{execute, transaction};
 use std::char::ToLowercase;
 use std::collections::HashMap;
 use std::fs::File;
@@ -7,56 +8,26 @@ use std::io;
 use std::io::{BufRead, BufReader};
 use std::string::ToString;
 
-fn insert(conn: &mut Connection, dbname: &String, word: String, freq: u32) {
-    conn.execute(
-        format!(
-            "INSERT INTO {name} (words, freq) VALUES (?1, ?2)",
-            name = dbname.replace(".", "")
-        )
-        .as_str(),
-        &[&word, &freq.to_string()],
-    )
-    .unwrap();
-}
-
 fn main() {
-    //IOinput
+    //init
     println!("Filename?");
     let mut filename = String::new();
     io::stdin()
         .read_line(&mut filename)
         .expect("Failed to read line");
-    //filter \n and other garbageinput
     let re = Regex::new(r"([0-9a-zA-Z\._-]+.(txt))").unwrap();
     filename = re.find(&filename).unwrap().as_str().to_string();
-    let dataname = filename.clone();
-    //verify input
-    println!("|{}|", filename.clone());
-    let file = File::open(filename).unwrap();
+    println!("|{}|", &filename);
+    //openfile
+    let file = File::open(&filename).unwrap();
     let reader = BufReader::new(file);
-    //
-    // startmapping
-    //
+    //mapping
     let mut frequency: HashMap<String, u32> = HashMap::new();
-    let mut conn = Connection::open("data.db").unwrap();
-    conn.execute(
-        format!(
-            "CREATE TABLE IF NOT EXISTS {name} (
-        id INTEGER PRIMARY KEY,
-        words TEXT,
-        freq INTEGER
-      )",
-            name = dataname.clone().replace(".", "")
-        )
-        .as_str(),
-        NO_PARAMS,
-    )
-    .unwrap();
-
+    
     for line in reader.lines() {
         let line = line.unwrap();
         //replace for sep.marks
-        &line
+        let tmpstr = line
             .to_lowercase()
             .replace(",", " ,")
             .replace(":", " :")
@@ -71,14 +42,36 @@ fn main() {
             .replace("«", " «")
             .replace("»", " »");
 
-        let out: Vec<&str> = line.split(" ").collect::<Vec<&str>>();
+        let out: Vec<&str> = tmpstr.split(" ").collect::<Vec<&str>>();
+
         for word in out {
             *frequency.entry(word.to_string()).or_insert(0) += 1;
         }
     }
-    println!("{:?}", frequency);
+    //SQL save
+    let mut conn = Connection::open("data.db").expect("Err");
+    dbinit(&mut conn,&filename);
+    dbpush(&mut conn,&filename, frequency);
 
-    for (s, fq) in frequency {
-        insert(&mut conn, &dataname, s, fq);
+    // for (s, fq) in frequency {
+            
+    // }
+
+
+
+}
+
+fn dbinit(db: &mut rusqlite::Connection, dbname: &String) {
+    let querry = format!("CREATE TABLE IF NOT EXISTS {name} (id INTEGER PRIMARY KEY, words TEXT, freq INTEGER)", name = dbname.replace(".", ""));
+    db.execute(querry.as_str(), [],).unwrap();
+}
+
+fn dbpush(db: &mut Connection, dbname: &String, freq: HashMap<String, u32>) {
+    let tx = db.transaction().expect("Err");
+    let querry = format!("INSERT INTO {name} (words, freq) VALUES (?1, ?2)", name = dbname.replace(".", ""));
+    for (s, fq) in freq {
+        tx.execute(&querry.as_str(), &[&s, &fq.to_string()],);
     }
+    tx.commit();
+
 }
